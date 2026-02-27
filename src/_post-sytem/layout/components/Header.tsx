6dc278bb939cde6a260e2d790b04c6e9567f5bea
CheaKimsan <cheakimsan456@gmail.com>
2026-02-27 11:40:42 +0700

    first commit

---
+++ src/_post-sytem/layout/components/Header.tsx
@@ -0,0 +1,6 @@
+const Header = () => {
+    return (
+        <h1>Hello Header</h1>
+    );
+}
+export default Header;
---
+++ src/app/routing/AppRoute.tsx
@@ -0,0 +1,9 @@
+import Header from "../../_post-sytem/layout/components/Header";
+
+const AppRoute: React.FC = () => {
+    return (
+        <Header />
+    );
+}
+
+export default AppRoute;
